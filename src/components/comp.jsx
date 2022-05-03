104bc40bbdfe7b3ff1738b8081745506a147fbbe
faizal8089 <faizalsulthan288@gmail.com>
2022-05-03 22:55:20 +0530

    init

---
+++ src/components/comp.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import logo from "../images/reactLogo.png";
+import logo2 from "../images/reactLogo2.png";
+
+function Navbar(){
+    return(
+        <nav className="nav">
+            <div className="logo">
+                <img className="reactlogo" src={logo} alt="React image" />
+                <h1 className="react_facts">ReactFacts</h1>
+            </div>
+            <h3 className="navDesc">React Course-Project 1</h3>
+        </nav>
+    );
+}
+
+function Main(){
+    return(
+        <div className="main">
+            <h1 className="facts">Fun Facts about React</h1>
+            <ul className="list">
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100k stars on GitHub</li>
+                <li>Is maintained by Facebook</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </div>
+    );
+}
+
+export {Navbar, Main};
